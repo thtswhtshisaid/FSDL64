2b43802715cd446d2a24bf6a06fc02a3eff91fd3
Ryan Reji <ryanrejir@gmail.com>
2025-03-19 04:54:39 +0000

    react3

--- week6/prog1/src/App.js
+++ week6/prog1/src/App.js
@@ -8,12 +8,9 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <div className="Main-content">
-        <Sidebar />
-        <div className="Content">
-          <h2>Welcome to the main content area!</h2>
-          <p>This is where your main content will go.</p>
-        </div>
+      <Sidebar />
+      <div className="Content">Content Area
+        <p>Hello</p>
       </div>
       <Footer />
     </div>
--- week6/prog1/src/module/sidebar.js
+++ week6/prog1/src/module/sidebar.js
@@ -1,13 +1,15 @@
-// src/Sidebar.js
+// src/module/Sidebar.js
 import React from 'react';
+import './style/sidebar.css'; // Import the sidebar CSS file
 
 function Sidebar() {
   return (
     <div className="Sidebar">
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
+      <ul className="sidebar-list">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
     </div>
   );
